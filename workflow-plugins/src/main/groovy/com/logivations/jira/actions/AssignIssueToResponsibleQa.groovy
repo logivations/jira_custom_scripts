7edbfe58d9536c7bb87f459f765dd51bb924c1e9
bohdan-petrovskyy <bohdan.petrovskyy@logivations.com>
2023-06-19 15:39:40 +0300

    - removed constrained related to Customer-specific issues - running of the plugin is now under bohdan.petrovskyy user

--- workflow-plugins/src/main/groovy/com/logivations/jira/actions/AssignIssueToResponsibleQa.groovy
+++ workflow-plugins/src/main/groovy/com/logivations/jira/actions/AssignIssueToResponsibleQa.groovy
@@ -17,26 +17,23 @@ String jiraUser = "bohdan.petrovskyy"
 // get 'Responsible QA' custom field
 CustomField responsibleQaCustomField = ComponentAccessor.getCustomFieldManager().getCustomFieldObject("customfield_11804")
 ApplicationUser responsibleQaValue = (ApplicationUser) issue.getCustomFieldValue(responsibleQaCustomField)
-CustomField developmentTypeCustomField = ComponentAccessor.getCustomFieldManager().getCustomFieldObject("customfield_11301")
-String developmentTypeValue = (String) issue.getCustomFieldValue(developmentTypeCustomField)
-if ("Customer specific" != developmentTypeValue) {
-    if (responsibleQaValue != null && !responsibleQaValue.name.contains("Not Available")) {
-        issue.setAssignee(responsibleQaValue)
-    } else {
-        def projectRoleService = ComponentAccessor.getComponent(ProjectRoleService)
-        def projectRoleManager = ComponentAccessor.getComponent(ProjectRoleManager)
-        ProjectRole projectRoleObject = projectRoleManager.getProjectRole("Lead QA")
-        Project project = issue.getProjectObject()
-        def errorCollection = new SimpleErrorCollection()
-        def actors = projectRoleService.getProjectRoleActors(projectRoleObject, project, errorCollection)
-        if(actors == null){
-            log.warn "projectRoleObject = " + projectRoleObject
-            log.warn "errorCollection = " + errorCollection
-            log.warn "project = " + project
-            return
-        }
-        ApplicationUser leadQaUser = (ApplicationUser) actors.getUsers().toArray()[0]
-        issue.setAssignee(leadQaUser)
-    }
+if (responsibleQaValue != null && !responsibleQaValue.name.contains("Not Available")) {
+	issue.setAssignee(responsibleQaValue)
+} else {
+	def projectRoleService = ComponentAccessor.getComponent(ProjectRoleService)
+	def projectRoleManager = ComponentAccessor.getComponent(ProjectRoleManager)
+	ProjectRole projectRoleObject = projectRoleManager.getProjectRole("Lead QA")
+	Project project = issue.getProjectObject()
+	def errorCollection = new SimpleErrorCollection()
+	def applicationUser = ComponentAccessor.getUserManager().getUserByKey(jiraUser)
+	def actors = projectRoleService.getProjectRoleActors(applicationUser, projectRoleObject, project, errorCollection)
+	if (actors == null) {
+		log.warn "projectRoleObject = " + projectRoleObject
+		log.warn "errorCollection = " + errorCollection
+		log.warn "project = " + project
+		return
+	}
+	ApplicationUser leadQaUser = (ApplicationUser) actors.getUsers().toArray()[0]
+	issue.setAssignee(leadQaUser)
 }
 
